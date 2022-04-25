a040b5c3c2db1357958870698e724d57d3606edf
Jan-Willem de Boer <janwillem@eacyt.nl>
2022-04-25 15:43:34 +0200

    Removed select2

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,15 +2,4 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-try {
-
-    window.$ = window.jQuery = require('jquery');
-
-    require('select2');
-    $('select').select2();
-
-} catch (error) {
-    console.log(error);
-}
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
